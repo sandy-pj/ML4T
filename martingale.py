1970e143dec4575df316fdd6950ba6745b54d0b7
Peilun Jiang <peilunjiang@Peiluns-MacBook-Pro.local>
2023-01-19 19:11:48 -0500

    name and id

--- martingale.py
+++ martingale.py
@@ -21,9 +21,9 @@ GT honor code violation.
   		  	   		  		 			  		 			     			  	 
 -----do not edit anything above this line---  		  	   		  		 			  		 			     			  	 
   		  	   		  		 			  		 			     			  	 
-Student Name: Tucker Balch (replace with your name)  		  	   		  		 			  		 			     			  	 
-GT User ID: tb34 (replace with your User ID)  		  	   		  		 			  		 			     			  	 
-GT ID: 900897987 (replace with your GT ID)  		  	   		  		 			  		 			     			  	 
+Student Name: Peilun Jiang (replace with your name)  		  	   		  		 			  		 			     			  	 
+GT User ID: pjiang49 (replace with your User ID)  		  	   		  		 			  		 			     			  	 
+GT ID: 903561681 (replace with your GT ID)  		  	   		  		 			  		 			     			  	 
 """  		  	   		  		 			  		 			     			  	 
   		  	   		  		 			  		 			     			  	 
 import numpy as np
